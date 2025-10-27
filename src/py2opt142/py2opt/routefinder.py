4f12c9aea092e3d038433c2f7388082ed168feac
Camilla Menon <menoncamilla.2000@gmail.com>
2025-10-27 13:00:10 +0100

    custom py2opt

--- src/py2opt142/py2opt/routefinder.py
+++ src/py2opt142/py2opt/routefinder.py
@@ -4,7 +4,7 @@ import time
 from py2opt.solver import Solver
 
 class RouteFinder:
-    def __init__(self, distance_matrix, cities_names, iterations=5, writer_flag=False, method='py2opt', return_to_begin=False, verbose=True):
+    def __init__(self, distance_matrix, cities_names, iterations=5, writer_flag=False, method='py2opt', return_to_begin=True, verbose=True):
         self.distance_matrix = distance_matrix
         self.iterations = iterations
         self.return_to_begin = return_to_begin
@@ -22,8 +22,8 @@ class RouteFinder:
 
         while iteration < self.iterations:
             num_cities = len(self.distance_matrix)
-            if self.verbose:
-                print(round(elapsed_time), 'msec')
+            # if self.verbose:
+                # print(round(elapsed_time), 'msec')
             initial_route = [0] + random2.sample(range(1, num_cities), num_cities - 1)
             if self.return_to_begin:
                 initial_route.append(0)
